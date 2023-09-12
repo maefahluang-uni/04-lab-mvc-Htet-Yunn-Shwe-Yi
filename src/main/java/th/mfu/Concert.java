168815fcd6df9f4341bb1bce774e1e13823bb861
Htet-Yunn-Shwe-Yi <122963018+Htet-Yunn-Shwe-Yi@users.noreply.github.com>
2023-09-12 13:18:57 +0000

    done 1

--- src/main/java/th/mfu/Concert.java
+++ src/main/java/th/mfu/Concert.java
@@ -7,55 +7,41 @@ import java.util.Date;
 public class Concert {
 
 
-    //TODO: add attributes
-    private int id;
-    private String title;
-    private String performer;
-    private Date date;
-    private String description;
+   //TODO: add attributes
+   private int id;
+   private String title;
+   private String performer;
+   private Date date;
+   private String description;
 
 
-    public Concert() {
-    }
-    public Concert(String title, String description) {
-        //TODO: set attributes
-        this.title = title;
-        this.description = description;
-    }
-  //TODO: add getters and setters for all attributes
-    // You can use Source action/generate getter setter function
-   
+   public Concert() {
+   }
+   public Concert(String title, String description) {
+       //TODO: set attributes
+       this.title = title;
+       this.description = description;
 
 
-    public void setId(int id) {
-        this.id = id;
-    }
-    public int getId() {
-        return id;
-    }
-    public String getTitle() {
-        return title;
-    }
-    public String getPerformer() {
-        return performer;
-    }
-    public Date getDate() {
-        return date;
-    }
-    public String getDescription() {
-        return description;
-    }
-    public void setTitle(String title) {
-        this.title = title;
-    }
-    public void setPerformer(String performer) {
-        this.performer = performer;
-    }
-    public void setDate(Date date) {
-        this.date = date;
-    }
-    public void setDescription(String description) {
-        this.description = description;
-    }
-   
+   }
+   //TODO: add getters and setters for all attributes
+   // You can use Source action/generate getter setter function
+   public int getId() {return id; }
+   public void setId(int id) {this.id = id;}
+
+
+   public String getTitle() {return title; }
+   public void setTitle(String title) {this.title = title;}
+
+
+   public String getPerformer() {return performer; }
+   public void setPerformer(String performer) {this.performer = performer;}
+
+
+   public String getDescription() {return description; }
+   public void setDescription(String description) {this.description = description;}
+
+
+   public Date getDate() {return date; }
+   public void setDate(Date date) {this.date = date;}
 }
